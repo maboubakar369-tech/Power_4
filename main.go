81d91abda26a1704a76b2a65b4dfcec73179b9dd
maboubakar369-tech <maboubakar369@gmail.com>
2025-10-16 15:56:10 +0100

    test

--- main.go
+++ main.go
@@ -3,7 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
-)
-func handleRequest(w http.Res
+
 }
-azlneaz, e
+func main() {
+    // sert /style/* (ex: /style/css.css)
+    http.Handle("/style/", http.StripPrefix("/", http.FileServer(http.Dir("."))))
+
+    http.HandleFunc("/", serveHome)
+    http.HandleFunc("/move", handleMove)
+
+    log.Println("✅ Serveur lancé sur http://localhost:8080")
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+hv v
